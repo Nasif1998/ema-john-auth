7d22cee775f1634305f439c5737004bb84476ac8
Nasif123 <nasif.reza@northsouth.edu>
2021-03-28 22:22:14 +0600

    yo

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const app = express()
 const port = 6001;
 
+app.get('/', (req, res) => {
+    res.send('hello from db');
+})
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({
